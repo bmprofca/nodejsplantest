5b6af57a2387b266fbb672a5c179a29d1610ac94
sourav1916 <souravadhikary1916@gmail.com>
2026-05-16 22:08:08 +0530

    Remove UUID generation from health check endpoint response

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require("express");
-const { v4: uuidv4 } = require('uuid');
-
 const app = express();
 const PORT = 3000;
 
 app.get("/health", (req, res) => {
     console.log("Checked at", new Date().toISOString());
     return res.json({
-        id: uuidv4(),
         health: "OK"
     });
 });
