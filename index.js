74c47ce9a37b1a40e890117fde91c2efcc5d7c02
sourav1916 <souravadhikary1916@gmail.com>
2026-05-16 22:06:25 +0530

    Add health check endpoint with UUID and logging

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = 3000;
 
 app.get("/health", (req, res) => {
-  return res.json({
-    health: "OK"
-  });
+    console.log("Checked at", new Date().toISOString());
+    return res.json({
+        id: uuidv4(),
+        health: "OK"
+    });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
